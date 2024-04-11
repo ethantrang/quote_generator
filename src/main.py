738d3bef818b40e4cb7b608481e7ca2be0845bfc
Ethan Trang <ethan.trang5521@gmail.com>
2024-04-11 10:52:28 +1000

    new update

---
+++ src/main.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI
+from logic import quote_client
+from pydantic import BaseModel
+from typing import List, Dict, Any
+
+app = FastAPI() 
+
+class QuoteRequest(BaseModel):
+    email: str
+    phone: str
+    first_name: str
+    last_name: str
+    location: str
+    gender: str
+
+    dob: List[int]
+    citizenship: str
+    income_bracket: int
+    vaccination: bool
+    smoking: bool
+    alcohol: bool
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.post("/quote")
+def get_quote(request: QuoteRequest):
+    quote_response = quote_client.calculate_quote(**request.dict())
+    return quote_response
+
+# @app.post("/details")
+# def get_details():
+#     #TODO: get details from db, check if quote submitted or not
+#     details = quote_client.get_details()
+#     return details
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
